54f0c1af1a45b76826ee7728f47cf1e6445f375d
YYY887 <ymy100861@qq.com>
2025-02-06 11:11:12 +0800

    Site updated: 2025-02-06 11:11:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/05/感悟/"];function toRandomPost(){
+var posts=["2025/02/05/感悟/","2025/02/06/开心/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
